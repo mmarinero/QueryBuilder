08a27a540bec7c7c1afdb7ad9a54eb861ae2bc63
Mario Marinero <mariomarinero@gmail.com>
2014-07-18 01:43:03 +0200

    PhpUnit tested and unit test stub created

---
+++ tests/QueryBuilderTest.php
@@ -0,0 +1,10 @@
+<?php
+class QueryBuilderTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testSelect()
+    {
+        // Assert
+        $this->assertEquals(true, true);
+    }
+}
